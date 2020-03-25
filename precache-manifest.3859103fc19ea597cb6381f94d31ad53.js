8028374cb677c71eaaebe411b40767387bfa939b
mdurazob <mdurazob@gmail.com>
2020-03-25 10:22:54 -0600

    Updates

---
+++ precache-manifest.3859103fc19ea597cb6381f94d31ad53.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84db5c2ab58a74cbaa8ced417241ea1e",
+    "url": "/covid2019/index.html"
+  },
+  {
+    "revision": "eb042ba3ae7b9dbb0328",
+    "url": "/covid2019/static/css/main.087de5b9.chunk.css"
+  },
+  {
+    "revision": "4a818f5a7bc855af2b82",
+    "url": "/covid2019/static/js/2.3ad0c41f.chunk.js"
+  },
+  {
+    "revision": "c344ff0e273d3dd0833ad3a0d4c0857e",
+    "url": "/covid2019/static/js/2.3ad0c41f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb042ba3ae7b9dbb0328",
+    "url": "/covid2019/static/js/main.aaa4a6fb.chunk.js"
+  },
+  {
+    "revision": "e57dd73081828872e1ad",
+    "url": "/covid2019/static/js/runtime-main.36e9d82c.js"
+  },
+  {
+    "revision": "1c057b131d41385e1dfde943e0483d69",
+    "url": "/covid2019/static/media/scene.1c057b13.svg"
+  }
+]);
